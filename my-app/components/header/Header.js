935172c38b67a30717adb8cee93bcf8f09a7d584
José Andrey de Melo Silva <enrimelo18@gmail.com>
2025-11-26 11:56:11 -0300

    Criado Header no /components e o Cadastro no /app

---
+++ my-app/components/header/Header.js
@@ -0,0 +1,26 @@
+import Link from 'next/link';
+import styles from './Header.module.css';
+
+export default function Header() {
+
+    return (
+        <header className={styles.header}>
+        <h1 className={styles.titulo}>
+            <Link href="/" className={styles.link}>
+            IFPB - Biblioteca
+            </Link>
+        </h1>
+        <nav>
+            <Link href="/" className={styles.link}>
+            Home
+            </Link>
+            <Link href="/cadastro" className={styles.link}>
+            Cadastro de Livros
+            </Link>
+            <Link href="/sobre" className={styles.link}>
+            Sobre
+            </Link>
+        </nav>
+        </header>
+    );
+}
